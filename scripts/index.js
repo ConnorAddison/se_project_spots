116d1d51b7934084dc2e4749c41aed605eb92598
Connor <connor.c.addison@gmail.com>
2025-06-03 11:17:43 -0700

    add card array to index.js line 1, add line 102 forEach that console logs card array element names

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,29 @@
-const initialCard0 = {
-  name: "An Aerial View of a Lush Green Forest - Author Unlisted",
-  link: "https://plus.unsplash.com/premium_photo-1688072514847-73bacc343aab?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-};
-
-const initialCard1 = {
-  name: "A Trail in the Woods With Lots of Trees - Tara-mae Miller",
-  link: "https://images.unsplash.com/photo-1731947463347-a6b36e97fe9d?q=80&w=1885&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-};
-
-const initialCard2 = {
-  name: "A Tent is Lit Up at Night in the Woods - Master Unknown",
-  link: "https://images.unsplash.com/photo-1725603080015-7d16a86c45d9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-};
-
-const initialCard3 = {
-  name: "Sea of Manazuru - Ryo Yoshitake",
-  link: "https://images.unsplash.com/photo-1512828539328-39f1b5a8bf54?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-};
-
-const initialCard4 = {
-  name: "Oregon :) - Arturo Rodriguez",
-  link: "https://images.unsplash.com/photo-1632297174075-88bf9f2ae509?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-};
-
-const initialCard5 = {
-  name: "Bridge Over Waterfalls - Ravi Pinisetti",
-  link: "https://images.unsplash.com/photo-1487734092093-e5b02908580e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-};
-
-// const initialCards = [
-//   "initialCard0",
-//   "initialCard1",
-//   "initialCard2",
-//   "initialCard3",
-//   "initialCard4",
-//   "initialCard5",
-// ];
+const initialCards = [
+  {
+    name: "An Aerial View of a Lush Green Forest - Author Unlisted",
+    link: "https://plus.unsplash.com/premium_photo-1688072514847-73bacc343aab?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "A Trail in the Woods With Lots of Trees - Tara-mae Miller",
+    link: "https://images.unsplash.com/photo-1731947463347-a6b36e97fe9d?q=80&w=1885&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "A Tent is Lit Up at Night in the Woods - Master Unknown",
+    link: "https://images.unsplash.com/photo-1725603080015-7d16a86c45d9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Sea of Manazuru - Ryo Yoshitake",
+    link: "https://images.unsplash.com/photo-1512828539328-39f1b5a8bf54?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Oregon :) - Arturo Rodriguez",
+    link: "https://images.unsplash.com/photo-1632297174075-88bf9f2ae509?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Bridge Over Waterfalls - Ravi Pinisetti",
+    link: "https://images.unsplash.com/photo-1487734092093-e5b02908580e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
 
 const profileEditButton = document.querySelector(".profile__edit-button");
 
@@ -110,3 +98,7 @@ function handleAddCardSubmit(evt) {
 }
 
 addCardFormElement.addEventListener("submit", handleAddCardSubmit);
+
+initialCards.forEach(function (card, index) {
+  console.log(card.name);
+});
